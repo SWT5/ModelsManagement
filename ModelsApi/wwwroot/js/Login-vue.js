2ed1f831627783847461a7f11e45798190f383f5
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-05-11 10:27:02 +0200

    added wwwroot and vue for login

---
+++ ModelsApi/wwwroot/js/Login-vue.js
@@ -0,0 +1,26 @@
+﻿
+vm = new Vue({
+	el: '#loginApp',
+	data: {
+		email: '',
+		password: ''
+	},
+	methods: {
+		loginApplication() {
+			let login = {};
+			login.email = this.email;
+			login.password = this.password;
+			fetch('/api/Account/login',
+				{
+					method: 'POST',
+					body: JSON.stringify(login),
+					headers: new Headers({
+						'Content-Type': 'application/json'
+					})
+				}).then(res => res.json().then((token) => {
+					localStorage.setItem("token", token.jwt);
+				}
+			).catch(error => console.error('Error:', error)));
+		}
+	}
+})
---
+++ ModelsApi/wwwroot/js/jwtCall.js
@@ -0,0 +1,15 @@
+﻿var url = "https://yourUrl";
+fetch(url, {
+	method: 'GET', //or POST, PUT, DELETE
+	credentials: 'include',
+	headers: {
+		'Authorization': 'Bearer ' + localStorage.getItem("token"),
+		'Content Type': 'application/json'
+	}
+	}).then(responseJson => {
+		var items = JSON.parse(responseJson);
+	})
+	.catch(error => ThisParameterType.setState({
+		isLoading: false,
+		message: 'Something bad happened ' + error
+	}));
